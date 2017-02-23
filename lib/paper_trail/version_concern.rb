5b391264ac7caa89e18d0bc7924e18ac3db67206
konami99 <richard@flatmates.com.au>
2017-02-23 14:55:06 +1100

    test18

--- lib/paper_trail/version_concern.rb
+++ lib/paper_trail/version_concern.rb
@@ -31,7 +31,7 @@ module PaperTrail
     module ClassMethods
       def with_item_keys(item_type, item_id)
         binding.pry
-        where(item_type: item_type, item_id: item_id).unscoped
+        where(item_type: item_type, item_id: item_id)
       end
 
       def creates
